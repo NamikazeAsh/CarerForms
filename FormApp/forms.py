e72b6843fc045d36bdc38093f225283a29f4e935
NamikazeAsh <ashwinsatish0@gmail.com>
2024-03-20 22:08:36 +0530

    logo, form data upload and load based on slug

--- FormApp/forms.py
+++ FormApp/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 from .models import *
 
-class DoctorForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    speciality = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    sub_speciality = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    qualification = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
-    experience = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    address = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
-    availability = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    additional_details = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model = Doctor
+        fields = ['name', 'speciality', 'sub_speciality', 'qualification', 'experience', 'address', 'availability', 'additional_details']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'speciality': forms.TextInput(attrs={'class': 'form-control'}),
+            'sub_speciality': forms.TextInput(attrs={'class': 'form-control'}),
+            'qualification': forms.TextInput(attrs={'class': 'form-control'}),
+            'experience': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'availability': forms.TextInput(attrs={'class': 'form-control'}),
+            'additional_details': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
