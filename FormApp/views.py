7b3a8a95d24d86d61ab6b0edb0c98ff6243411f7
NamikazeAsh <ashwinsatish0@gmail.com>
2024-03-21 00:30:27 +0530

    generate form link

--- FormApp/views.py
+++ FormApp/views.py
@@ -5,11 +5,16 @@ import uuid
 from django.utils.text import slugify
 from django.db import IntegrityError
 
-def generate_form_link(request):
-    form_slug = str(uuid.uuid4())[:8]
-    
-    return redirect('open_form', form_slug=form_slug)
 
+
+def generate_form_link(request):
+    
+    if request.method == 'POST':    
+        form_slug = str(uuid.uuid4())[:8]
+        return redirect('open_form', form_slug=form_slug)
+    else:
+        return render(request,"home.html")
+    
 def open_form(request, form_slug):
     try:
         doctor = get_object_or_404(Doctor, slug=form_slug)
@@ -34,3 +39,17 @@ def open_form(request, form_slug):
 def confirm_form(request, form_slug):
     # Implement OTP confirmation logic here
     return render(request, 'confirmation.html')
+
+
+# Twilio OTP logic
+# otp = str(random.randint(1000, 9999))
+#                 client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+#                 try:
+#                     message = client.messages.create(body=f'Your OTP is: {otp}',from_=settings.TWILIO_PHONE_NUMBER,to="+91"+email)
+#                     #Storing in session
+#                     request.session['otp'] = otp
+#                     request.session['email'] = email
+#                     return JsonResponse({'success': True})
+                
+#                 except Exception as e:
+#                     return JsonResponse({'success': False, 'error': str(e)})
